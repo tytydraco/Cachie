0e97f4c889e534ac672cfba4b6ea1194e8f3dedd
Tyler Nijmeh <tylernij@gmail.com>
2021-04-02 22:09:58 -0700

    Add functionality
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/cachie/fragments/MainPreferenceFragment.kt
+++ app/src/main/java/com/draco/cachie/fragments/MainPreferenceFragment.kt
@@ -11,6 +11,13 @@ import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.material.snackbar.Snackbar
 
 class MainPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+    private lateinit var reset: Preference
+    private lateinit var sysStorageThresholdPercentage: EditTextPreference
+    private lateinit var sysStorageThresholdMaxBytes: EditTextPreference
+    private lateinit var sysStorageFullThresholdBytes: EditTextPreference
+    private lateinit var sysStorageCachePercentage: EditTextPreference
+    private lateinit var sysStorageCacheMaxBytes: EditTextPreference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main, rootKey)
     }
@@ -28,11 +35,20 @@ class MainPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnS
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        reset = findPreference(getString(R.string.pref_action_key_reset))!!
+        sysStorageThresholdPercentage = findPreference(getString(R.string.pref_config_key_sys_storage_threshold_percentage))!!
+        sysStorageThresholdMaxBytes = findPreference(getString(R.string.pref_config_key_sys_storage_threshold_max_bytes))!!
+        sysStorageFullThresholdBytes = findPreference(getString(R.string.pref_config_key_sys_storage_full_threshold_bytes))!!
+        sysStorageCachePercentage = findPreference(getString(R.string.pref_config_key_sys_storage_cache_percentage))!!
+        sysStorageCacheMaxBytes = findPreference(getString(R.string.pref_config_key_sys_storage_cache_max_bytes))!!
+
         refreshSettings()
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         when (preference.key) {
+            getString(R.string.pref_action_key_reset) -> resetSettings()
+
             getString(R.string.pref_developer_key) -> openURL(getString(R.string.developer_url))
             getString(R.string.pref_source_key) -> openURL(getString(R.string.source_url))
             getString(R.string.pref_contact_key) -> openURL(getString(R.string.contact_url))
@@ -46,15 +62,40 @@ class MainPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnS
     }
 
     /**
+     * Put everything back to defaults
+     */
+    private fun resetSettings() {
+        val contentResolver = requireContext().contentResolver
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_PERCENTAGE, null)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_MAX_BYTES, null)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_FULL_THRESHOLD_BYTES, null)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_PERCENTAGE, null)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_MAX_BYTES, null)
+        refreshSettings()
+    }
+
+    /**
      * Update the UI to show the new constants
      */
     private fun refreshSettings() {
+        val contentResolver = requireContext().contentResolver
+        sysStorageThresholdPercentage.text = Settings.Global.getString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_PERCENTAGE)
+        sysStorageThresholdMaxBytes.text = Settings.Global.getString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_MAX_BYTES)
+        sysStorageFullThresholdBytes.text = Settings.Global.getString(contentResolver, SettingsConstants.SYS_STORAGE_FULL_THRESHOLD_BYTES)
+        sysStorageCachePercentage.text = Settings.Global.getString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_PERCENTAGE)
+        sysStorageCacheMaxBytes.text = Settings.Global.getString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_MAX_BYTES)
     }
 
     /**
      * Take the UI settings and apply them as constants
      */
     private fun applySettings() {
+        val contentResolver = requireContext().contentResolver
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_PERCENTAGE, sysStorageThresholdPercentage.text)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_THRESHOLD_MAX_BYTES, sysStorageThresholdMaxBytes.text)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_FULL_THRESHOLD_BYTES, sysStorageFullThresholdBytes.text)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_PERCENTAGE, sysStorageCachePercentage.text)
+        Settings.Global.putString(contentResolver, SettingsConstants.SYS_STORAGE_CACHE_MAX_BYTES, sysStorageCacheMaxBytes.text)
     }
 
     /**
--- app/src/main/java/com/draco/cachie/repositories/constants/SettingsConstants.kt
+++ app/src/main/java/com/draco/cachie/repositories/constants/SettingsConstants.kt
@@ -1,4 +1,9 @@
 package com.draco.cachie.repositories.constants
 
 object SettingsConstants {
+    const val SYS_STORAGE_THRESHOLD_PERCENTAGE =    "sys_storage_threshold_percentage"
+    const val SYS_STORAGE_THRESHOLD_MAX_BYTES =     "sys_storage_threshold_max_bytes"
+    const val SYS_STORAGE_FULL_THRESHOLD_BYTES =    "sys_storage_full_threshold_bytes"
+    const val SYS_STORAGE_CACHE_PERCENTAGE =        "sys_storage_cache_percentage"
+    const val SYS_STORAGE_CACHE_MAX_BYTES =         "sys_storage_cache_max_bytes"
 }
